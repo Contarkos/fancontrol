8941f3ea84c3c1f237b812bdb6c34be158f63c75
Martin <martin.teulon@cegetel.net>
2020-01-27 23:23:06 +0100

    Refactoring pour fan et mode OFF sur PWM en exit

--- modules/fan/src/fan_class.cpp
+++ modules/fan/src/fan_class.cpp
@@ -61,77 +61,60 @@ int FAN::start_module()
     int ret = 0;
     char s[] = FAN_SOCKET_NAME;
 
-    LOG_INF1("FAN : Démarrage de la classe du module");
+    LOG_INF1("FAN : Starting module class");
 
     /* Démarrage du timer pour la boucle */
-    this->timer_id = OS_create_timer_msg(FAN_TIMER_USEC, OS_TIMER_PERIODIC, FAN_TIMER);
+    if (0 == ret)
+    {
+        this->timer_id = OS_create_timer_msg(FAN_TIMER_USEC, OS_TIMER_PERIODIC, FAN_TIMER);
 
-    if (0 > this->timer_id)
-    {
-        LOG_ERR("FAN : erreur création timer de boucle");
-        ret = -1;
+        if (0 > this->timer_id)
+        {
+            LOG_ERR("FAN : error while creating looping timer");
+            ret = -1;
+        }
     }
-    else
+
+    /* Set de la diode en entrée pour lire la vitesse */
+    if (0 == ret)
     {
-        /* Set de la diode en entrée pour lire la vitesse */
         ret = OS_set_gpio(FAN_PIN_IN, OS_GPIO_FUNC_IN);
 
         if (ret != 0)
             LOG_ERR("FAN : error setting GPIO input, ret = %d", ret);
     }
 
+    /* Set de la pin en sortie pour le controle de l'allumage */
     if (0 == ret)
     {
-        /* Set de la pin en sortie pour le controle de l'allumage */
         ret = OS_set_gpio(FAN_PIN_OUT, OS_GPIO_FUNC_OUT);
 
         if (ret != 0)
             LOG_ERR("FAN : error setting GPIO output, ret = %d", ret);
     }
 
+    /* Set de la diode en sortie en PWM */
     if (0 == ret)
     {
-        /* Set de la diode en sortie en PWM */
-        ret += OS_set_gpio(FAN_PIN_PWM, OS_GPIO_FUNC_ALT5);
+        ret = OS_set_gpio(FAN_PIN_PWM, OS_GPIO_FUNC_ALT5);
 
         if (ret != 0)
             LOG_ERR("FAN : error setting GPIO PWM function, ret = %d", ret);
     }
 
+    /* Configuration of the PWM Hardware */
     if (0 == ret)
     {
-        /* Reglage source Clock sur PLL C */
-        ret += OS_pwm_set_clock_source(OS_CLOCK_SRC_PLLC);
+        ret = fan_set_pwm();
 
-        /* Mode MS pour le PWM */
-        ret += OS_pwm_set_mode(OS_PWM_MODE_MSMODE);
-
-        /* Cycle par défaut : 1024 */
-        ret += OS_pwm_set_precision(FAN_DEFAULT_PREC);
-
-        /* Reglage du filter MASH par défaut */
-        ret += OS_pwm_set_mash(OS_PWM_MASH_FILTER_1);
-
-        /* Reglage de la fréquence PWM */
-        ret += OS_pwm_set_frequency(FAN_PWM_FREQ);
-
-        /* Reglage du duty cycle par defaut : 50% */
-        ret += OS_pwm_set_dutycycle(FAN_DEFAULT_CYCLE);
-
-        /* Activation... */
-        ret += OS_pwn_enable(OS_STATE_ON);
-
-        if (ret < 0)
-        {
-            LOG_ERR("FAN : erreur lors du parametrage du PWM, ret = %d", ret);
-            return ret;
-        }
+        if (ret)
+            LOG_ERR("FAN : error while setting up PWM hardware, ret = %d", ret);
     }
 
     if (0 == ret)
     {
         /* Creation socket */
-        LOG_INF1("FAN : création de la socket d'écoute");
+        LOG_INF1("FAN : creating listening UNIX socket");
 
         /* Ouverture socket UNIX */
         this->socket_fd = COM_create_socket(AF_UNIX, SOCK_DGRAM, 0, s, sizeof(s));
@@ -143,23 +126,24 @@ int FAN::start_module()
         }
         else
         {
-            LOG_ERR("FAN : erreur à la création de la socket");
+            LOG_ERR("FAN : error creating UNIX socket");
             ret = -1;
         }
     }
 
     if (0 == ret)
     {
-        /* Requete interruption */
+        /* Request interruption */
         this->irq_fd = OS_irq_request(OS_IRQ_TIME_NAME, O_RDONLY);
 
         if (0 == this->irq_fd)
         {
-            LOG_ERR("FAN : erreur requete interruption pour lecture vitesse fan");
+            LOG_ERR("FAN : error requesting interruption for reading fan speed");
             ret = -4;
         }
         else
         {
+            LOG_INF3("FAN : request for IRQ OK, fd = %d", this->irq_fd);
             this->p_fd[FAN_FD_IRQ].fd = this->irq_fd;
         }
     }
@@ -179,10 +163,13 @@ int FAN::start_module()
             this->p_fd[FAN_FD_COM].fd = this->fan_semfd;
 
             ret = COM_msg_subscribe_array(COM_ID_FAN, fan_msg_array, fan_msg_array_size);
+
+            if (ret)
+                LOG_ERR("FAN : error while subscribing to messages, ret = %d", ret);
         }
     }
 
-    LOG_INF3("FAN : fin init module (ret = %d)", ret);
+    LOG_INF3("FAN : end of module init (ret = %d)", ret);
     return ret;
 }
 
@@ -191,16 +178,28 @@ int FAN::init_after_wait(void)
 {
     int ret = 0;
 
-    /* Demarrage du timer */
-    ret = OS_start_timer(this->timer_id);
+    if (0 == ret)
+    {
+        /* Demarrage du timer */
+        ret = OS_start_timer(this->timer_id);
 
-    if (ret < 0)
-        LOG_ERR("FAN : timer non démarré, ret = %d", ret);
+        if (ret < 0)
+            LOG_ERR("FAN : timer not started, ret = %d", ret);
+    }
+
+    if (0 == ret)
+    {
+        /* Activation PWM... */
+        ret = OS_pwn_enable(OS_STATE_ON);
+
+        if (ret)
+            LOG_ERR("FAN : error while configuring PWM, ret = %d", ret);
+    }
 
     if (0 == ret)
     {
         /* Allumage du fan */
-        this->fan_set_power(FAN_POWER_MODE_ON);
+        ret = fan_set_power(FAN_POWER_MODE_ON);
     }
 
     return ret;
@@ -212,18 +211,46 @@ int FAN::stop_module()
     int ret = 0;
 
     /* Arret du ventilateur */
-    ret += this->fan_set_power(FAN_POWER_MODE_OFF);
+    {
+        ret = this->fan_set_power(FAN_POWER_MODE_OFF);
+
+        if (ret)
+            LOG_ERR("FAN : error while stopping fan, ret = %d", ret);
+    }
+
+    /* TODO : Stop PWM properly */
+    {
+        ret = OS_pwn_enable(OS_STATE_OFF);
+
+        if (ret)
+            LOG_ERR("FAN : error while stopping PWM, ret = %d", ret);
+    }
 
     /* Arret du timer */
-    ret += OS_stop_timer(this->timer_id);
+    {
+        ret = OS_stop_timer(this->timer_id);
+
+        if (ret)
+            LOG_ERR("FAN : error while stopping timer, ret = %d", ret);
+    }
 
     /* Fermeture de la socket */
-    ret += COM_close_socket(this->socket_fd);
+    {
+        ret = COM_close_socket(this->socket_fd);
+
+        if (ret)
+            LOG_ERR("FAN : error while closing socket, ret = %d", ret);
+    }
 
     /* Fermeture IRQ */
-    ret += OS_irq_close(this->irq_fd);
+    {
+        ret = OS_irq_close(this->irq_fd);
 
-    LOG_INF3("FAN : fin du stop_module, ret = %d", ret);
+        if (ret)
+            LOG_ERR("FAN : error while closing IRQ, ret = %d", ret);
+    }
+
+    LOG_INF3("FAN : end of stop_module, ret = %d", ret);
     return ret;
 }
 
@@ -260,7 +287,7 @@ int FAN::exec_loop()
                         break;
                     case FAN_FD_NB:
                     default:
-                        LOG_WNG("FAN : mauvais file descriptor");
+                        LOG_WNG("FAN : bad file descriptor, index = %d", ii);
                         ret = 2;
                         break;
                 }
