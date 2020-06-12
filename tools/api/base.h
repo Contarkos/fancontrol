eb01114bb82ca1ee33b993515bc64ba09b98822a
Martin <martin.teulon@cegetel.net>
2020-06-12 18:25:15 +0200

    Better practice on usual macros

--- tools/api/base.h
+++ tools/api/base.h
@@ -1,7 +1,7 @@
 #ifndef BASE_H_
 #define BASE_H_
 
-/* Définition de types */
+/* Types definitions */
 typedef char t_int8;
 typedef unsigned char t_uint8;
 typedef short t_int16;
@@ -10,19 +10,25 @@ typedef int t_int32;
 typedef unsigned int t_uint32;
 typedef volatile int v_int32;
 typedef volatile unsigned int v_uint32;
+typedef float t_float32;
+typedef double t_float64;
 typedef long long int t_int64;
 typedef unsigned long long int t_uint64;
 
+/* Enum definitions */
+typedef enum {
+    BASE_FALSE = 0,
+    BASE_TRUE = 1
+} base_bool;
+
 /* Fichier contenant les macros transverses */
 
-#define UNUSED_PARAMS(x)        ((void) x)
-#define BASE_MAX(a, b)          (a > b ? a : b)
-#define BASE_MIN(a, b)          (a < b ? a : b)
-#define BASE_BORNE(a, min, max) (a > min ? (a < max ? a : max) : min)
+#define UNUSED_PARAMS(x)        ((void) (x))
+#define BASE_MAX(a, b)          ((a) > (b) ? (a) : (b))
+#define BASE_MIN(a, b)          ((a) < (b) ? (a) : (b))
+#define BASE_BORNE(a, min, max) ((a) > (min) ? ((a) < (max) ? (a) : (max)) : (min))
 
 #define MAX_UINT_16         65535
-
-#define BASE_OK             0
 
 #endif /* BASE_H_ */
 
