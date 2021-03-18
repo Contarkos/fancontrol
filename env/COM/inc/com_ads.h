8b9087fcb7708495e7a6c34fd47f17ffd32f3e48
Martin <martin.teulon@cegetel.net>
2021-03-18 22:26:30 +0100

    Adding a private header for COM ADS

---
+++ env/COM/inc/com_ads.h
@@ -0,0 +1,66 @@
+#ifndef COM_ADS_H_
+#define COM_ADS_H_
+
+/*****************************************************************************/
+/*                                  Defines                                  */
+/*****************************************************************************/
+
+#define COM_ADS1115_ADDRESS             0x48
+
+#define COM_ADS_DEFAULT_SETUP           0x8483
+
+/* Registers addresses */
+#define COM_ADS_CONVERSION_REGISTER     0x00
+#define COM_ADS_CONFIG_REGISTER         0x01
+#define COM_ADS_LO_THRESH_REGISTER      0x02
+#define COM_ADS_HI_THRESH_REGISTER      0x03
+
+#define COM_ADS_CONVERSION_SIZE         2U
+#define COM_ADS_CONFIG_SIZE             2U
+#define COM_ADS_LO_THRESH_SIZE          2U
+#define COM_ADS_HI_THRESH_SIZE          2U
+
+#define COM_ADS_POINTER_SIZE            1U
+
+/* Masks and shift for Conversion Register */
+
+/* Masks and shift for Config Register */
+#define COM_ADS_CONFIG_COMP_MASK        (0x0003)
+#define COM_ADS_CONFIG_COMP_SHIFT       0U
+#define COM_ADS_CONFIG_COMP_LATCH_MASK  (0x0004)
+#define COM_ADS_CONFIG_COMP_LATCH_SHIFT 2U
+#define COM_ADS_CONFIG_COMP_POL_MASK    (0x0008)
+#define COM_ADS_CONFIG_COMP_POL_SHIFT   3U
+#define COM_ADS_CONFIG_COMP_MODE_MASK   (0x0010)
+#define COM_ADS_CONFIG_COMP_MODE_SHIFT  4U
+#define COM_ADS_CONFIG_DR_MASK          (0x00E0)
+#define COM_ADS_CONFIG_DR_SHIFT         5U
+#define COM_ADS_CONFIG_MODE_MASK        (0x0100)
+#define COM_ADS_CONFIG_MODE_SHIFT       8U
+#define COM_ADS_CONFIG_PGA_MASK         (0x0E00)
+#define COM_ADS_CONFIG_PGA_SHIFT        9U
+#define COM_ADS_CONFIG_MUX_MASK         (0x7000)
+#define COM_ADS_CONFIG_MUX_SHIFT        12U
+#define COM_ADS_CONFIG_OS_MASK          (0x8000)
+#define COM_ADS_CONFIG_OS_SHIFT         15U
+
+typedef union
+{
+    t_uint16 setup;
+    struct
+    {
+        t_uint16 comp:2;
+        t_uint16 comp_latch:1;
+        t_uint16 comp_pol:1;
+        t_uint16 comp_mode:1;
+        t_uint16 rate:3;
+        t_uint16 mode:1;
+        t_uint16 pga:3;
+        t_uint16 mux:3;
+        t_uint16 os:1;
+    } bits;
+} t_com_ads_config;
+
+/* Masks and shift for Lo_Thresh and Hi_Thresh registers */
+
+#endif /* COM_ADS_H_ */
